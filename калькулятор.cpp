31b8488d0ce41b1ba0bfea7cbe7e1f93cfb10986
Artem0451 <81007839+Artem0451@users.noreply.github.com>
2021-04-19 17:18:10 +0300

    Add files via upload

---
+++ калькулятор.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+using namespace std;
+
+class calkulator {
+private:
+	float num_1, num_2;
+	
+public:
+	int plus(int x, int y) {
+		x = x + y;
+		return x;
+	}
+	int minus(int x, int y) {
+		x = x - y;
+		return x;
+	}
+	void setnum(int x, int y) {
+		x = num_1;
+		y = num_2;
+	}
+};
+class calkulator_son :public calkulator {
+public:
+	int dilint(int x, int y) {
+		x = x % y;
+		return x;
+	}
+	int div(int x, int y) {
+		x = x / y;
+		return x;
+	}
+	int mul(int x, int y) {
+		x = x * y;
+		return x;
+	}
+};
+
+int main()
+{
+	float num_1, num_2;
+	cin >> num_1 >> num_2;
+	calkulator_son ex;
+	int plus,minus,mul,div,dilint,divdiv;
+	ex.setnum(num_1, num_2);
+	plus = ex.plus(num_1, num_2);
+	cout << plus << endl;
+	minus = ex.minus(num_1, num_2);
+	cout << minus << endl;
+	mul = ex.mul(num_1, num_2);
+	cout << mul << endl;
+	div = ex.div(num_1, num_2);
+	cout << div << endl;
+	dilint = ex.dilint(num_1, num_2);
+	cout << dilint << endl;
+}
